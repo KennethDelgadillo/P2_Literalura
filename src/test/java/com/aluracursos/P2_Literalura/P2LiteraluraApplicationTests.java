08021d928d1d085cc21c42685797e3419f939944
KennethDelgadillo <kennethdelgadillo@gmail.com>
2024-07-14 13:24:32 -0600

    Initial commit

---
+++ src/test/java/com/aluracursos/P2_Literalura/P2LiteraluraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aluracursos.P2_Literalura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class P2LiteraluraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
